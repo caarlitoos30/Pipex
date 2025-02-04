9b2555ac6312c2512f107d06e87333b2ffad668d
caarlitoos30 <caarlitoos30@gmail.com>
2025-02-04 08:19:37 +0100

    pequeño avanze de clase

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: calguaci <calguaci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 23:05:48 by calguaci          #+#    #+#             */
-/*   Updated: 2025/02/03 23:48:05 by calguaci         ###   ########.fr       */
+/*   Updated: 2025/02/04 08:18:17 by calguaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 /*Ahora tenemos que recibir un archivo, un comando que se aplica sobre este y que el output lo
 redireccione al input de la segunda parte del pipe, y que luego el output del segundo comando del pipe se redireccione,
 a un output file que seleccionamos **Dudosa posibilidad del bonus debido a la futura minishell** */
+
+/* ./pipex infile < grep -a | wc -c > outfile*/
 int main(int argc, char **argv, char **env)
 {
     int p_fd[2];
